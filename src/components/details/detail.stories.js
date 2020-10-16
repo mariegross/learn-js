754297e518bcf20d4277ef394160d5474e051392
mariegross <marie.gross@gmx.de>
2020-10-16 15:55:29 +0200

    Create details component

---
+++ src/components/details/detail.stories.js
@@ -0,0 +1,9 @@
+import "./details.css";
+import { createDetailElement } from "./details";
+
+export default {
+  title: "Components/details",
+  parameters: { layout: "centered" },
+};
+
+export const departureDetail = () => createDetailElement();
